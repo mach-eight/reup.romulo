35733183d31a8ef35365d53b0444b50f3ec37340
Sebastian <jsebdev@gmail.com>
2024-07-24 18:34:19 -0500

    remove object info controller from building setup dependency injector

--- Runtime/DependencyInjectors/SetUpBuildingDependencyInjector.cs
+++ Runtime/DependencyInjectors/SetUpBuildingDependencyInjector.cs
@@ -13,7 +13,6 @@ namespace ReupVirtualTwin.dependencyInjectors
             SetupBuilding setUpBuilding = GetComponent<SetupBuilding>();
             setUpBuilding.colliderAdder = new ColliderAdder();
             setUpBuilding.idAssignerController = new IdController();
-            setUpBuilding.objectInfoController = new ObjectInfoController();
         }
     }
 }
