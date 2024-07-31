237b433e7c5682df7150f543ee24d9c0d12d703b
Sebastian <jsebdev@gmail.com>
2024-07-31 13:13:19 -0500

    update to version 1.1.8

--- Runtime/Managers/ModelInfoManager.cs
+++ Runtime/Managers/ModelInfoManager.cs
@@ -17,7 +17,7 @@ namespace ReupVirtualTwin.managers
         public string buildVersion { get => _buildVersion; }
         public IObjectMapper objectMapper { set => _objectMapper = value; }
 
-        string _buildVersion = "2024-06-06";
+        string _buildVersion = "2024-07-31"; // format: YYYY-MM-DD
         IOnBuildingSetup setupBuilding;
         IObjectMapper _objectMapper;
 
