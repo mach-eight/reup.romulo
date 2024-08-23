8085005506b46f1201e3a2bf4cd4363e182a74c8
Sebastian <jsebdev@gmail.com>
2024-08-23 16:20:18 -0500

    test MaterialScaler

--- Tests/PlayMode/MaterialScalerTest.cs
+++ Tests/PlayMode/MaterialScalerTest.cs
@@ -1,19 +1,20 @@
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 using UnityEditor;
-using System.Collections;
 using ReupVirtualTwin.helpers;
 
 namespace ReupVirtualTwinTests.helpers
 {
-    public class UvUtilsTest
+    public class MaterialScalerTest
     {
         GameObject wallPrefabInM = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/com.reup.romulo/Tests/TestAssets/1_5mx1_5m_inclined_texture_0_1mx0_14m.prefab");
         GameObject wallPrefabInCm = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/com.reup.romulo/Tests/TestAssets/150cmx150cm_inclined_texture_10cmx14cm.prefab");
         GameObject wallInstanceInM;
         GameObject wallInstanceInCm;
         Vector2 originalTextureDimensions;
+        MaterialScaler materialScaler;
+        Vector2 desiredTextureDimensionsInM;
+        Vector2 expectedMaterialScale;
 
         [SetUp]
         public void SetUp()
@@ -21,6 +22,11 @@ namespace ReupVirtualTwinTests.helpers
             wallInstanceInCm = (GameObject)PrefabUtility.InstantiatePrefab(wallPrefabInCm);
             wallInstanceInM = (GameObject)PrefabUtility.InstantiatePrefab(wallPrefabInM);
             originalTextureDimensions = new Vector2(0.1f, 0.14f);
+            float xScale = 5;
+            float yScale = 10;
+            desiredTextureDimensionsInM = new Vector2(originalTextureDimensions.x * xScale, originalTextureDimensions.y * yScale);
+            expectedMaterialScale = new Vector2(1/xScale, 1/yScale);
+            materialScaler = new MaterialScaler();
         }
         [TearDown]
         public void TearDown()
@@ -30,43 +36,17 @@ namespace ReupVirtualTwinTests.helpers
         }
 
         [Test]
-        public void GetTextureDimensions_success_for_fbx_imported_in_cm()
-        {
-            Vector2 textureDimensions = UvUtils.GetTextureDimensions(wallInstanceInCm);
-            AssertUtils.AssertVector2sAreEqual(originalTextureDimensions, textureDimensions);
-        }
-
-        [Test]
         public void AdjustUVScaleToDimensions_success_for_fbx_imported_in_cm()
         {
-            float xScale = 5;
-            float yScale = 10;
-            Vector2 desiredTextureDimensionsInM = new Vector2(
-                originalTextureDimensions.x * xScale, originalTextureDimensions.y * yScale
-            );
-            Vector2 expectedMaterialScale = new Vector2(1/xScale, 1/yScale);
-            UvUtils.AdjustUVScaleToDimensions(wallInstanceInCm, desiredTextureDimensionsInM);
+            materialScaler.AdjustUVScaleToDimensions(wallInstanceInCm, desiredTextureDimensionsInM);
             Material material = wallInstanceInCm.GetComponent<Renderer>().material;
             AssertUtils.AssertVector2sAreEqual(expectedMaterialScale, material.mainTextureScale);
         }
 
         [Test]
-        public void GetTextureDimensions_success_for_fbx_imported_in_m()
-        {
-            Vector2 textureDimensions = UvUtils.GetTextureDimensions(wallInstanceInM);
-            AssertUtils.AssertVector2sAreEqual(originalTextureDimensions, textureDimensions);
-        }
-
-        [Test]
         public void AdjustUVScaleToDimensions_success_for_fbx_imported_in_m()
         {
-            float xScale = 5;
-            float yScale = 10;
-            Vector2 desiredTextureDimensionsInM = new Vector2(
-                originalTextureDimensions.x * xScale, originalTextureDimensions.y * yScale
-            );
-            Vector2 expectedMaterialScale = new Vector2(1/xScale, 1/yScale);
-            UvUtils.AdjustUVScaleToDimensions(wallInstanceInM, desiredTextureDimensionsInM);
+            materialScaler.AdjustUVScaleToDimensions(wallInstanceInM, desiredTextureDimensionsInM);
             Material material = wallInstanceInM.GetComponent<Renderer>().material;
             AssertUtils.AssertVector2sAreEqual(expectedMaterialScale, material.mainTextureScale);
         }
