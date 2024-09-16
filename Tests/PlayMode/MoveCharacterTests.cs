05abe0b408607a9ffa24e3032a1d828fe71a4241
Sebastian <jsebdev@gmail.com>
2024-09-16 13:34:32 -0500

    Test ASDW keys to move character

---
+++ Tests/PlayMode/MoveCharacterTests.cs
@@ -0,0 +1,92 @@
+using NUnit.Framework;
+using ReupVirtualTwin.behaviours;
+using ReupVirtualTwinTests.utils;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TestTools;
+
+namespace ReupVirtualTwinTests.behaviours
+{
+    public class MoveCharacterTests : InputTestFixture
+    {
+        ReupSceneInstantiator.SceneObjects sceneObjects;
+        Keyboard keyboard;
+        Transform characterTransform;
+
+        float timeInSecsForEachAction = 0.25f;
+
+        public override void Setup()
+        {
+            base.Setup();
+            sceneObjects = ReupSceneInstantiator.InstantiateScene();
+            keyboard = InputSystem.AddDevice<Keyboard>();
+            characterTransform = sceneObjects.character.transform;
+        }
+        public override void TearDown()
+        {
+            ReupSceneInstantiator.DestroySceneObjects(sceneObjects);
+            base.TearDown();
+        }
+
+        [Test]
+        public void WalkSpeedIsDefined()
+        {
+            Assert.AreEqual(3.5f, CharacterMovementKeyboard.WALK_SPEED_M_PER_SECOND);
+        }
+
+        [UnityTest]
+        public IEnumerator WKeyShouldMoveCharacterForward()
+        {
+            Assert.AreEqual(Vector3.zero, characterTransform.position);
+            Press(keyboard.wKey);
+            yield return new WaitForSeconds(timeInSecsForEachAction);
+            Release(keyboard.wKey);
+            Assert.GreaterOrEqual(characterTransform.position.z, CharacterMovementKeyboard.WALK_SPEED_M_PER_SECOND * timeInSecsForEachAction);
+            Assert.Zero(characterTransform.position.x);
+            Assert.Zero(characterTransform.position.y);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator AKeyShouldMoveCharacterToTheLeft()
+        {
+            Assert.AreEqual(Vector3.zero, characterTransform.position);
+            Press(keyboard.aKey);
+            yield return new WaitForSeconds(timeInSecsForEachAction);
+            Release(keyboard.aKey);
+            Assert.LessOrEqual(characterTransform.position.x, -1 * CharacterMovementKeyboard.WALK_SPEED_M_PER_SECOND * timeInSecsForEachAction);
+            Assert.Zero(characterTransform.position.z);
+            Assert.Zero(characterTransform.position.y);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator DKeyShouldMoveCharacterToTheRight()
+        {
+            Assert.AreEqual(Vector3.zero, characterTransform.position);
+            Press(keyboard.dKey);
+            yield return new WaitForSeconds(timeInSecsForEachAction);
+            Release(keyboard.dKey);
+            Assert.GreaterOrEqual(characterTransform.position.x, CharacterMovementKeyboard.WALK_SPEED_M_PER_SECOND * timeInSecsForEachAction);
+            Assert.Zero(characterTransform.position.z);
+            Assert.Zero(characterTransform.position.y);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator SKeyShouldMoveCharacterBackwards()
+        {
+            Assert.AreEqual(Vector3.zero, characterTransform.position);
+            Press(keyboard.sKey);
+            yield return new WaitForSeconds(timeInSecsForEachAction);
+            Release(keyboard.sKey);
+            Assert.LessOrEqual(characterTransform.position.z, -1 * CharacterMovementKeyboard.WALK_SPEED_M_PER_SECOND * timeInSecsForEachAction);
+            Assert.Zero(characterTransform.position.x);
+            Assert.Zero(characterTransform.position.y);
+            yield return null;
+        }
+
+
+    }
+}
