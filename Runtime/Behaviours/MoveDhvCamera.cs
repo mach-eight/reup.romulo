70995a62261bd954faa2ecf63000ebbb384e3939
Sebastian <jsebdev@gmail.com>
2024-09-16 10:12:36 -0500

    move dhv camera in horizontal plane

---
+++ Runtime/Behaviours/MoveDhvCamera.cs
@@ -0,0 +1,41 @@
+using ReupVirtualTwin.inputs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReupVirtualTwin.behaviours
+{
+    public class MoveDhvCamera : MonoBehaviour
+    {
+        [SerializeField]
+        Transform dhvCameraTransformHandler;
+
+        InputProvider _inputProvider;
+        float MOVE_CAMERA_SPEED_M_PER_SECOND = 3.5f;
+
+        void Awake()
+        {
+            _inputProvider = new InputProvider();
+        }
+
+        void Update()
+        {
+            UpdatePosition();
+        }
+
+        void UpdatePosition()
+        {
+            Vector2 inputValue = _inputProvider.MovementInput().normalized;
+            PerformMovement(inputValue);
+        }
+
+        void PerformMovement(Vector2 direction)
+        {
+            Vector3 directionIn3D = new Vector3(direction.x, 0, direction.y);
+            Vector3 normalizedDirection = Vector3.Normalize(directionIn3D);
+            dhvCameraTransformHandler.position = dhvCameraTransformHandler.position
+                + normalizedDirection * MOVE_CAMERA_SPEED_M_PER_SECOND * Time.deltaTime;
+        }
+
+    }
+}
