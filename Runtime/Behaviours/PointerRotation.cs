fbfe3012d392b6eb877c7f5a856170c7e503ea9a
Sebastian <jsebdev@gmail.com>
2024-10-17 18:31:49 -0500

    test more accurate

--- Runtime/Behaviours/PointerRotation.cs
+++ Runtime/Behaviours/PointerRotation.cs
@@ -19,10 +19,15 @@ namespace ReupVirtualTwin.behaviours
         IGesturesManager gesturesManager;
         Vector3 startDragRayDirection;
         Vector3 startCameraForwardDirection;
-        float startHorizontalRotationAngle;
+        // float startHorizontalRotationAngle;
         Vector3 startRayDirProjectedToHorizontalPlane;
-        Vector3 startRayDirProjectedToVerticalPlane;
+        // Vector3 startRayDirProjectedToVerticalPlane;
         Vector3 startDragRayLeftDirection;
+
+        private void Start()
+        {
+            Debug.Log("empezando");
+        }
 
         [Inject]
         public void Init(InputProvider inputProvider, IDragManager dragManager, IGesturesManager gesturesManager)
@@ -48,51 +53,61 @@ namespace ReupVirtualTwin.behaviours
             startDragRayDirection = startDragRay.direction;
             Debug.Log($"48: startDragRayDirection >>>\n{startDragRayDirection}");
             startCameraForwardDirection = Camera.main.transform.forward;
-            startHorizontalRotationAngle = _characterRotationManager.horizontalRotation;
+            // startHorizontalRotationAngle = _characterRotationManager.horizontalRotation;
             startRayDirProjectedToHorizontalPlane = Vector3.Normalize(Vector3.ProjectOnPlane(startDragRayDirection, Vector3.up));
             Debug.Log($"52: startRayDirProjectedToHorizontalPlane >>>\n{startRayDirProjectedToHorizontalPlane}");
-            startDragRayLeftDirection = Vector3.Normalize(Vector3.Cross(startRayDirProjectedToHorizontalPlane, startDragRayDirection) * startDragRayDirection.y);
-            Debug.Log($"54: startCharacterRightDirection >>>\n{startDragRayLeftDirection}");
-            startRayDirProjectedToVerticalPlane = Vector3.Normalize(Vector3.ProjectOnPlane(startDragRayDirection, startDragRayLeftDirection));
-            Debug.Log($"56: startRayDirProjectedToVerticalPlane >>>\n{startRayDirProjectedToVerticalPlane}");
+            float sideFactor = startDragRayDirection.y >= 0 ? 1 : -1;
+            Debug.Log($"55: sideFactor >>>\n{sideFactor}");
+            startDragRayLeftDirection = Vector3.Normalize(Vector3.Cross(startRayDirProjectedToHorizontalPlane, startDragRayDirection) * sideFactor);
+            if (Vector3.Magnitude(startDragRayLeftDirection) == 0)
+            {
+                startDragRayLeftDirection = Vector3.Normalize(Vector3.Cross(startRayDirProjectedToHorizontalPlane, Vector3.up));
+            }
+            Debug.Log($"54: startDragRayLeftDirection >>>\n{startDragRayLeftDirection}");
+            // startRayDirProjectedToVerticalPlane = Vector3.Normalize(Vector3.ProjectOnPlane(startDragRayDirection, startDragRayLeftDirection));
+            // Debug.Log($"56: startRayDirProjectedToVerticalPlane >>>\n{startRayDirProjectedToVerticalPlane}");
         }
 
         void Update()
         {
+            Debug.Log("update pointer rotation");
             if (!dragManager.dragging || gesturesManager.gestureInProgress)
             {
                 return;
             }
+            Debug.Log($"77: Camera.main.transform.forward >>>\n{Camera.main.transform.forward}");
             Ray dragRay = Camera.main.ScreenPointToRay(inputProvider.PointerInput());
             Debug.DrawRay(dragRay.origin, dragRay.direction * 100, Color.red);
             Debug.Log($"55: dragRay.direction >>>\n{dragRay.direction}");
             float horizontalRotationAngle = GetHorizontalRotationAngle(dragRay.direction);
-            float verticalRotationAngle = GetVerticalRotationAngle(dragRay.direction);
-            // Debug.Log($"61: horizontalRotationAngle >>>\n{horizontalRotationAngle}");
+            Debug.Log($"61: horizontalRotationAngle >>>\n{horizontalRotationAngle}");
+            Debug.Log($"77: _characterRotationManager.horizontalRotation >>>\n{_characterRotationManager.horizontalRotation}");
             _characterRotationManager.horizontalRotation += horizontalRotationAngle;
-            Debug.Log($"71: verticalRotationAngle >>>\n{verticalRotationAngle}");
-            Debug.Log($"78: _characterRotationManager.verticalRotation >>>\n{_characterRotationManager.verticalRotation}");
+            Debug.Log($"79: _characterRotationManager.horizontalRotation >>>\n{_characterRotationManager.horizontalRotation}");
+            float verticalRotationAngle = GetVerticalRotationAngle(dragRay.direction);
+            // Debug.Log($"71: verticalRotationAngle >>>\n{verticalRotationAngle}");
+            // Debug.Log($"78: _characterRotationManager.verticalRotation >>>\n{_characterRotationManager.verticalRotation}");
             _characterRotationManager.verticalRotation += verticalRotationAngle;
-            Debug.Log($"80: _characterRotationManager.verticalRotation >>>\n{_characterRotationManager.verticalRotation}");
+            // Debug.Log($"80: _characterRotationManager.verticalRotation >>>\n{_characterRotationManager.verticalRotation}");
 
         }
 
         float GetHorizontalRotationAngle(Vector3 currentDragRayDirection)
         {
             Vector3 currentRayProjectedToHorizontal = Vector3.ProjectOnPlane(currentDragRayDirection, Vector3.up);
-            // Debug.Log($"74: startRayDirectionProjected >>>\n{startRayDirProjectedToHorizontalPlane}");
-            // Debug.Log($"72: currentRayDirectionProjected >>>\n{currentRayProjectedToHorizontal}");
+            Debug.Log($"74: startRayDirectionProjected >>>\n{startRayDirProjectedToHorizontalPlane}");
+            Debug.Log($"72: currentRayDirectionProjected >>>\n{currentRayProjectedToHorizontal}");
             float angle = Vector3.SignedAngle(startRayDirProjectedToHorizontalPlane, currentRayProjectedToHorizontal, Vector3.up);
-            // Debug.Log($"76: angle >>>\n{angle}");
+            Debug.Log($"76: angle >>>\n{angle}");
             return -angle;
         }
 
         float GetVerticalRotationAngle(Vector3 currentDragRayDirection)
         {
             Vector3 currentRayProjectedToVertical = Vector3.ProjectOnPlane(currentDragRayDirection, startDragRayLeftDirection);
-            Debug.Log($"93: startRayDirProjectedToVerticalPlane >>>\n{startRayDirProjectedToVerticalPlane}");
-            Debug.Log($"89: currentRayProjectedToVertical >>>\n{currentRayProjectedToVertical}");
-            float angle = Vector3.SignedAngle(startRayDirProjectedToVerticalPlane, currentRayProjectedToVertical, startDragRayLeftDirection);
+            // Debug.Log($"93: startRayDirProjectedToVerticalPlane >>>\n{startRayDirProjectedToVerticalPlane}");
+            // Debug.Log($"89: currentRayProjectedToVertical >>>\n{currentRayProjectedToVertical}");
+            float angle = Vector3.SignedAngle(startDragRayDirection, currentRayProjectedToVertical, startDragRayLeftDirection);
             return angle;
         }
 
--- Runtime/Managers/CharacterRotationManager.cs
+++ Runtime/Managers/CharacterRotationManager.cs
@@ -4,15 +4,14 @@ namespace ReupVirtualTwin.managers
 {
     public class CharacterRotationManager : MonoBehaviour, ICharacterRotationManager
     {
-        float ROTATION_SPEED = 10f;
+        // float ROTATION_SPEED = 10f;
         float ANGLE_THRESHOLD = 0.01f;
         float _verticalRotation = 0f;
         float _horizontalRotation = 0f;
         Quaternion _desiredInnerRotation;
         Quaternion _desiredHorizontalRotation;
 
-        [SerializeField]
-        Transform _innerCharacterTransform;
+        [SerializeField] Transform _innerCharacterTransform;
 
         bool _allowRotation = true;
         public bool allowRotation
@@ -57,6 +56,7 @@ namespace ReupVirtualTwin.managers
 
         void Update()
         {
+            Debug.Log("update character rotation");
             if (ShouldRotate())
             {
                 Rotate();
@@ -64,7 +64,9 @@ namespace ReupVirtualTwin.managers
         }
         void SetDesiredHorizontalRotation()
         {
+            // Debug.Log($"67: _horizontalRotation >>>\n{_horizontalRotation}");
             _desiredHorizontalRotation = Quaternion.Euler(0, _horizontalRotation, 0);
+            // Debug.Log($"69: _desiredHorizontalRotation >>>\n{_desiredHorizontalRotation}");
         }
         void SetDesiredInnerRotation()
         {
@@ -74,25 +76,37 @@ namespace ReupVirtualTwin.managers
         bool ShouldRotate()
         {
             var shouldRotateVertically = Quaternion.Angle(_desiredInnerRotation, _innerCharacterTransform.rotation) > ANGLE_THRESHOLD;
+            // Debug.Log($"79: shouldRotateVertically >>>\n{shouldRotateVertically}");
+            // Debug.Log($"77: Quaternion.Angle(_desiredHorizontalRotation, transform.rotation) >>>\n{Quaternion.Angle(_desiredHorizontalRotation, transform.rotation)}");
             var shouldRotateHorizontally = Quaternion.Angle(_desiredHorizontalRotation, transform.rotation) > ANGLE_THRESHOLD;
+            // Debug.Log($"79: shouldRotateHorizontally >>>\n{shouldRotateHorizontally}");
             return shouldRotateVertically || shouldRotateHorizontally;
         }
 
         void Rotate()
         {
-            var rotationStep = ROTATION_SPEED * Time.deltaTime;
-            RotateCharacter(rotationStep);
-            RotateInnerCharacter(rotationStep);
-        }
-        void RotateInnerCharacter(float rotationStep)
-        {
+            Debug.Log($"88: transform.forward >>>\n{transform.forward}");
+            Debug.Log($"88: Camera.main.transform.forward >>>\n{Camera.main.transform.forward}");
+            Debug.Log("rotating");
+            // var rotationStep = ROTATION_SPEED * Time.deltaTime;
             SetDesiredInnerRotation();
-            _innerCharacterTransform.rotation = Quaternion.Slerp(_innerCharacterTransform.rotation, _desiredInnerRotation, rotationStep);
+            _innerCharacterTransform.rotation = _desiredInnerRotation;
+            transform.rotation = _desiredHorizontalRotation;
+            Debug.Log($"93: transform.forward >>>\n{transform.forward}");
+            Debug.Log($"95: Camera.main.transform.forward >>>\n{Camera.main.transform.forward}");
         }
-        void RotateCharacter(float rotationStep)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, _desiredHorizontalRotation, rotationStep);
-        }
+        // void RotateInnerCharacter(float rotationStep)
+        // {
+        //     // _innerCharacterTransform.rotation = Quaternion.Slerp(_innerCharacterTransform.rotation, _desiredInnerRotation, rotationStep);
+        //     // _innerCharacterTransform.rotation = _desiredInnerRotation;
+        // }
+        // void RotateCharacter(float rotationStep)
+        // {
+        //     Debug.Log($"99: transform.rotation >>>\n{transform.rotation}");
+        //     // transform.rotation = Quaternion.Slerp(transform.rotation, _desiredHorizontalRotation, rotationStep);
+        //     transform.rotation = _desiredHorizontalRotation;
+        //     Debug.Log($"102: transform.rotation >>>\n{transform.rotation}");
+        // }
     }
 
 }
