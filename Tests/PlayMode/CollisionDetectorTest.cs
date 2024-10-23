8b52573d0549ca4c9941d432d5a57097df7b15d3
juanfersierra <juanfer@macheight.com>
2024-10-23 10:13:13 -0500

    Add collider to character and create test for collisions

--- Tests/PlayMode/CollisionDetectorTest.cs
+++ Tests/PlayMode/CollisionDetectorTest.cs
@@ -7,6 +7,7 @@ using System.Collections;
 using ReupVirtualTwin.managers;
 using ReupVirtualTwinTests.utils;
 using ReupVirtualTwin.managerInterfaces;
+using UnityEngine.InputSystem;
 
 public class CollisionDetectorTest : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class CollisionDetectorTest : MonoBehaviour
     GameObject cubePrefab;
     GameObject widePlatform;
     GameObject wall;
+    Keyboard keyboard;
+    InputTestFixture input;
 
     [SetUp]
     public void SetUp()
@@ -27,6 +30,8 @@ public class CollisionDetectorTest : MonoBehaviour
         posManager = sceneObjects.characterPositionManager;
         wall = (GameObject)PrefabUtility.InstantiatePrefab(cubePrefab);
         widePlatform = (GameObject)PrefabUtility.InstantiatePrefab(cubePrefab);
+        keyboard = InputSystem.AddDevice<Keyboard>();
+        input = sceneObjects.input;
         SetPlatform();
         SetWallAt2MetersInZAxis();
     }
@@ -50,6 +55,14 @@ public class CollisionDetectorTest : MonoBehaviour
 
         yield return new WaitForSeconds(2f);
 
+        Assert.LessOrEqual(character.transform.position.z, 2);
+    }
+    [UnityTest]
+    public IEnumerator CharacterShouldNotCrossWallWhileMovingWithKeyboard()
+    {
+        input.Press(keyboard.wKey);
+        yield return new WaitForSeconds(1);
+        input.Release(keyboard.wKey);
         Assert.LessOrEqual(character.transform.position.z, 2);
     }
     private void SetPlatform()
