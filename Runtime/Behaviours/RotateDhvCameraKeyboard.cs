3a3304e7ae11a8d1735f4aeebbde2a9e9e63515b
juanfersierra <juanfer@macheight.com>
2024-10-25 13:44:43 -0500

    Change prefab and and scripts for rotation

---
+++ Runtime/Behaviours/RotateDhvCameraKeyboard.cs
@@ -0,0 +1,43 @@
+using ReupVirtualTwin.inputs;
+using UnityEngine;
+using Zenject;
+
+namespace ReupVirtualTwin.behaviours
+{
+    public class RotateDhvCameraKeyboard : MonoBehaviour
+    {
+        [SerializeField] public Transform dollhouseViewWrapper;
+        [SerializeField] public float keyboardRotationSpeedDegreesPerSecond = 80f;
+
+        private float maxVerticalAngle = 89.9f;
+        private float minVerticalAngle = 10f;
+        private InputProvider _inputProvider;
+
+        [Inject]
+        public void Init(InputProvider inputProvider)
+        {
+            _inputProvider = inputProvider;
+        }
+
+        private void Update()
+        {
+            UpdateCameraRotationWithKeyboard();
+        }
+
+        private void UpdateCameraRotationWithKeyboard()
+        {
+            Vector2 rotationDirection = _inputProvider.KeyboardRotateDhvCamera();
+            if (rotationDirection == Vector2.zero)
+            {
+                return;
+            }
+            float deltaSpeed = keyboardRotationSpeedDegreesPerSecond * Time.deltaTime;
+            float horizontalRotation = dollhouseViewWrapper.localEulerAngles.y - (rotationDirection.x * deltaSpeed);
+            float verticalRotation = dollhouseViewWrapper.localEulerAngles.x - (rotationDirection.y * deltaSpeed * -1);
+            verticalRotation = Mathf.Clamp(verticalRotation, minVerticalAngle, maxVerticalAngle);
+            dollhouseViewWrapper.localEulerAngles = new Vector3(verticalRotation, horizontalRotation, 0);
+        }
+
+    }
+
+}
---
+++ Runtime/Behaviours/RotateDhvCameraMouse.cs
@@ -0,0 +1,48 @@
+using ReupVirtualTwin.helpers;
+using ReupVirtualTwin.inputs;
+using ReupVirtualTwin.managerInterfaces;
+using UnityEngine;
+using Zenject;
+
+namespace ReupVirtualTwin.behaviours
+{
+    public class RotateDhvCameraMouse : MonoBehaviour
+    {
+        [SerializeField] public Transform dollhouseViewWrapper;
+        private float maxVerticalAngle = 89.9f;
+        private float minVerticalAngle = 10f;
+        InputProvider _inputProvider;
+        IDragManager dragManager;
+
+        [Inject]
+        public void Init(IDragManager dragManager, InputProvider inputProvider)
+        {
+            _inputProvider = inputProvider;
+            this.dragManager = dragManager;
+        }
+        private void Update()
+        {
+            if (dragManager.secondaryDragging)
+            {
+                UpdateCameraRotationWithMouse();
+            }
+        }
+
+        private void UpdateCameraRotationWithMouse()
+        {
+            Vector2 mouseDelta = _inputProvider.MouseRotateDhvCamera();
+
+            float horizontalRotation = (mouseDelta.x / Screen.width) * 360f;
+            float verticalRotation = (mouseDelta.y / Screen.height) * 180f;
+
+            float currentVerticalAngle = dollhouseViewWrapper.localEulerAngles.x;
+            float newVerticalAngle = Mathf.Clamp(currentVerticalAngle - verticalRotation, minVerticalAngle, maxVerticalAngle);
+
+            dollhouseViewWrapper.localEulerAngles = new Vector3(
+                newVerticalAngle,
+                dollhouseViewWrapper.localEulerAngles.y + horizontalRotation,
+                0
+            );
+        }
+    }
+}
