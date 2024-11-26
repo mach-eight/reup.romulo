7d8e3a039a1d6cb0aaf723952ee7a12957acaca6
juanfersierra <juanfer@macheight.com>
2024-11-26 16:18:47 -0500

    Enhance texture compression by enabling mipmaps and setting filter mode to avoid visual artifacts

--- Runtime/Helpers/TextureCompresser.cs
+++ Runtime/Helpers/TextureCompresser.cs
@@ -7,9 +7,12 @@ namespace ReupVirtualTwin.helpers
     {
         public Texture2D GetASTC12x12CompressedTexture(Texture2D texture)
         {
-            Texture2D compressedTexture = new Texture2D(texture.width, texture.height, TextureFormat.ASTC_12x12, false);
+            Texture2D compressedTexture = new Texture2D(texture.width, texture.height, TextureFormat.ASTC_12x12, true);
             byte[] textureBytes = texture.EncodeToPNG();
             ImageConversion.LoadImage(compressedTexture, textureBytes);
+            compressedTexture.Apply(true, true);
+            compressedTexture.filterMode = FilterMode.Trilinear;
+            compressedTexture.mipMapBias = -0.5f;
             return compressedTexture;
         }
     }
